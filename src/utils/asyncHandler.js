e1c34ef16a2381f41c8bfe1b9aa8b53b9a79f524
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-12-07 19:41:50 +0530

    all bug fixed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,30 +1,27 @@
-// this is wrapper function
-
-// #1 - using promises
 const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err)); //use chatgpt to understand this code
-  };
-};
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
-export { asyncHandler };
 
-// #2 - using trycatch
+export { asyncHandler }
 
-// const asyncHandler = ()=>{}
-// const asyncHandler = (func)=>{const subFunc=()=>{}}
-// remove curly braces and make function anonymous
-// const asyncHandler = (func)=>()=>{}
-// make it async
-// const asyncHandler = (func)=>async()=>{}
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
 
 // const asyncHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req, res, next);
-//   } catch (error) {
-//     res.status(err.code || 500).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-// };
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
